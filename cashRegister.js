26eff818f2a813d1c8d9223913261fcb7ac92e07
julin <juchuanlin@hotmail.com>
2019-01-10 14:50:52 -0800

    freeCodeCamp JavaScript Algorithms and Data Structures Project: Cash Register

---
+++ cashRegister.js
@@ -0,0 +1,118 @@
+function getChanges(change, cid){
+	var iter = moneyMap.entries();
+	var val = iter.next().value;
+	var cidMap = new Map(cid);
+	var info = {status:"", change:[]};
+	if (change >= val[1]) {
+	//more than one humdred dollars
+	//do nothing
+	} else {
+		while (change < val[1]){
+			val = iter.next().value;
+		}};
+	let count = 0;
+	while (change > 0){
+		if (typeof val === "undefined"){
+			info.status = "INSUFFICIENT_FUNDS";
+			info.change = [];
+			return (info);
+		}
+		let drawerCash = cidMap.get(val[0]);
+		let remaining = (change - drawerCash).toFixed(2);
+		if (remaining > 0)
+		{
+			change = remaining;
+			info.change.push ([val[0], drawerCash]);
+		} else if (remaining === 0)
+		{
+			change -= remaining;
+			change = change.toFixed(2);
+			info.status = 'OPEN';
+			info.change.push([val[0], drawerCash]);
+		}
+		else {
+			//remaining < 0
+			//toFixed will return string
+			let neededCash = Number(((Math.floor(change/val[1])) * val[1]).toFixed(2));
+			if (neededCash > 0){
+				change = Number((change - neededCash).toFixed(2));
+				info.change.push([val[0], neededCash]);
+			}
+		}
+		val = iter.next().value;
+	}
+	if (info.status === ""){
+		info.status = 'OPEN';
+	}
+
+	return info;
+}
+
+
+
+
+
+
+const moneyMap = new Map([["ONE HUNDRED", 100], ["TWENTY", 20], ["TEN", 10], ["FIVE", 5], ["ONE", 1],
+			["QUARTER", 0.25], ["DIME", 0.1], ["NICKEL",0.05],["PENNY", 0.01]]);
+
+
+
+
+
+
+function checkCashRegister(price, cash, cid){
+	console.log("price = ", price, ", cash = ", cash);
+	var info = {status:"none", change:[]};
+	
+
+	var totalMoney = cid.reduce(function(accumulator, currentValue){
+		accumulator += currentValue[1];
+		return accumulator;
+	},0);
+
+	var change = cash - price;
+	if (totalMoney === change){
+		info.status = "CLOSED";
+		info.change = cid;
+		return info;
+	}
+	if (totalMoney < change){
+		info.status = "INSUFFICIENT_FUNDS";
+		info.change = [];
+		return info;
+	}
+	info = getChanges(change.toFixed(2), cid);
+	return(info);
+}
+
+console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], 
+	["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], 
+	["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+
+console.log("========================================================================");
+
+console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], 
+	["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], 
+	["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+
+console.log("========================================================================");
+
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], 
+	["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], 
+	["TWENTY", 0], ["ONE HUNDRED", 0]]));
+
+console.log("========================================================================");
+
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], 
+	["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], 
+	["TWENTY", 0], ["ONE HUNDRED", 0]]));
+
+console.log("========================================================================");
+
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], 
+	["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], 
+	["TWENTY", 0], ["ONE HUNDRED", 0]]));
+
+console.log("========================================================================");
+
